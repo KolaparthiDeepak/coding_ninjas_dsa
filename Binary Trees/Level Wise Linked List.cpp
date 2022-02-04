b8df93891946f4ead8ea7e176b207a9a30f131a9
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2022-02-04 11:47:31 +0530

    Create Level Wise Linked List.cpp

---
+++ Binary Trees/Level Wise Linked List.cpp
@@ -0,0 +1,109 @@
+/*Given a binary tree, write code to create a separate linked list for each level. You need to return the array which contains head of each level linked list.
+Input format :
+
+The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
+
+Output format :
+
+Each level linked list is printed in new line (elements are separated by space).
+
+Constraints:
+
+Time Limit: 1 second
+
+Sample Input 1:
+
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+
+Sample Output 1:
+
+5 
+6 10 
+2 3 
+9
+*/
+
+#include <vector>
+#include <queue>
+/**********************************************************
+
+	Following are the Binary Tree Node class structure and
+	the Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public: 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this->data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+	template <typename T>
+	class Node {
+		public:
+    	T data;
+    	Node<T> *next;
+    	Node(T data) {
+        	this->data=data;
+        	this->next=NULL;
+    	}
+	};
+
+***********************************************************/
+
+vector<Node<int>*> constructLinkedListForEachLevel(BinaryTreeNode<int> *root) {
+    // Write your code here
+    
+    if(root==NULL)
+        return {};
+    vector<Node<int>*> v;
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    q.push(NULL);
+    Node<int> *head=NULL;
+    Node<int> *tail=NULL;
+    while(q.size()!=0)
+    {
+        auto front=q.front();
+        q.pop();
+        if(front==NULL)
+        {
+            v.push_back(head);
+            head=NULL;
+            tail=NULL;
+            if(q.size()==0)
+            {
+                break;
+            }
+            q.push(NULL);
+            continue;
+        }
+        if(front->left!=NULL)
+        {
+            q.push(front->left);
+        }
+        if(front->right!=NULL)
+        {
+            q.push(front->right);
+        }
+        Node<int> *newnode=new Node<int>(front->data);
+        if(head!=NULL)
+        {
+            tail->next=newnode;
+            tail=newnode;
+        }
+        else
+        {
+            head=newnode;
+            tail=newnode;
+        }
+    }
+    return v;
+}
+
