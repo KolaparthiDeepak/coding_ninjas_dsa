581aa50af558e5f477dfa273ea38edde4f555d49
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2022-02-16 17:14:15 +0530

    Create Sort ksorted Array.cpp

---
+++ Priority Queues/Sort ksorted Array.cpp
@@ -0,0 +1,28 @@
+//sort the array which is ksorted...
+
+#include <iostream>
+using namespace std;
+#include <queue>
+
+void kSortedArray(int input[], int n, int k) {
+	priority_queue<int> pq;
+	for(int i = 0; i < k; i++) {
+		pq.push(input[i]);
+	}
+
+	int j = 0;
+	for(int i = k; i < n; i++) {
+		input[j] = pq.top();
+		pq.pop();
+		pq.push(input[i]);
+		j++;
+	}
+
+
+	while(!pq.empty()) {
+		input[j] = pq.top();
+		pq.pop();
+    j++;
+	}
+
+}
