3fa18f3d9e043922fd728c9d89b94ac950ab8f63
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2022-03-02 18:13:07 +0530

    Update Implementation of  hashmap.cpp

--- Hashmaps/Implementation of  hashmap.cpp
+++ Hashmaps/Implementation of  hashmap.cpp
@@ -8,60 +8,62 @@ class MapNode {
 		V value;
 		MapNode* next;
 
-		MapNode(string key, V value) 
-    {
+		MapNode(string key, V value) {
 			this->key = key;
 			this->value = value;
 			next = NULL;
 		}
 
-		~MapNode() 
-    {
+		~MapNode() {
 			delete next;
 		}
 };
 
 template <typename V>
-class ourmap 
-{
+class ourmap {
 	MapNode<V>** buckets;
 	int count;
 	int numBuckets;
 
 	public:
-	ourmap() 
-  {
+	ourmap() {
 		count = 0;
 		numBuckets = 5;
 		buckets = new MapNode<V>*[numBuckets];
-		for (int i = 0; i < numBuckets; i++) 
-    {
+		for (int i = 0; i < numBuckets; i++) {
 			buckets[i] = NULL;
 		}
 	}
 
-	~ourmap() 
-  {
-		for (int i = 0; i < numBuckets; i++) 
-    {
+	~ourmap() {
+		for (int i = 0; i < numBuckets; i++) {
 			delete buckets[i];
 		}
 		delete [] buckets;
 	}
 
-	int size() 
-  {
+	int size() {
 		return count;
 	}
-  
-  private:
-	int getBucketIndex(string key) 
-  {
+
+	V getValue(string key) {
+		int bucketIndex = getBucketIndex(key);
+		MapNode<V>* head = buckets[bucketIndex];
+		while (head != NULL) {
+			if (head->key == key) {
+				return head->value;
+			}
+			head = head->next;
+		}
+		return 0;
+	}
+
+	private:
+	int getBucketIndex(string key) {
 		int hashCode = 0;
 
 		int currentCoeff = 1;
-		for (int i = key.length() - 1; i >= 0; i--) 
-    {
+		for (int i = key.length() - 1; i >= 0; i--) {
 			hashCode += key[i] * currentCoeff;
 			hashCode = hashCode % numBuckets;
 			currentCoeff *= 37;
@@ -72,29 +74,11 @@ class ourmap
 	}
 
 	public:
-	V getValue(string key) 
-  {
+	void insert(string key, V value) {
 		int bucketIndex = getBucketIndex(key);
 		MapNode<V>* head = buckets[bucketIndex];
-		while (head != NULL) 
-    {
-			if (head->key == key) 
-      {
-				return head->value;
-			}
-			head = head->next;
-		}
-		return 0;
-	}
-
-	void insert(string key, V value) 
-  {
-		int bucketIndex = getBucketIndex(key);
-		MapNode<V>* head = buckets[bucketIndex];
-		while (head != NULL) 
-    {
-			if (head->key == key) 
-      {
+		while (head != NULL) {
+			if (head->key == key) {
 				head->value = value;
 				return;
 			}
@@ -107,21 +91,15 @@ class ourmap
 		count++;
 	}
 
-	V remove(string key) 
-  {
+	V remove(string key) {
 		int bucketIndex = getBucketIndex(key);
 		MapNode<V>* head = buckets[bucketIndex];
 		MapNode<V>* prev = NULL;
-		while (head != NULL)
-    {
-			if (head->key == key) 
-      {
-				if (prev == NULL) 
-        {
+		while (head != NULL) {
+			if (head->key == key) {
+				if (prev == NULL) {
 					buckets[bucketIndex] = head->next;
-				} 
-        else 
-        {
+				} else {
 					prev->next = head->next;
 				}
 				V value = head->value;
