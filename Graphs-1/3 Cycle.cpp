62e19521ed8573a67abf0d6b2d68550359cef8bd
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2022-03-17 12:13:23 +0530

    Create 3 Cycle.cpp

---
+++ Graphs-1/3 Cycle.cpp
@@ -0,0 +1,76 @@
+/*
+Given a graph with N vertices (numbered from 0 to N-1) and M undirected edges, then count the distinct 3-cycles in the graph. A 3-cycle PQR is a cycle in which (P,Q), (Q,R) and (R,P) are connected by an edge.
+Input Format :
+
+The first line of input contains two space separated integers, that denotes the value of N and M.
+Each of the following M lines contain two integers, that denote the vertices which have an undirected edge between them. Let us denote the two vertices with the symbol u and v. 
+
+Output Format :
+
+Print the count the number of 3-cycles in the given graph
+
+Constraints :
+
+0 <= N <= 100
+0 <= M <= (N*(N-1))/2
+0 <= u <= N - 1
+0 <= v <= N - 1
+Time Limit: 1 sec
+
+Sample Input 1:
+
+3 3
+0 1
+1 2
+2 0
+
+Sample Output 1:
+
+1
+*/
+
+#include <iostream>
+using namespace std;
+int solve(int **edges,int n)
+{
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(edges[i][j]!=0 && i!=j)
+            {
+                for(int k=0;k<n;k++)
+                {
+                    if(j!=k && k!=i && edges[j][k]!=0 && edges[i][k]!=0)
+                        count++;
+                }
+            }
+        }
+    }
+    return count/6;
+}
+int main() {
+    // Write your code here
+    int n;
+    int e;
+    cin >> n >> e;
+    int** edges = new int*[n];
+    for(int i=0;i<n;i++)
+    {
+        edges[i]=new int[n];
+        for(int j=0;j<n;j++)
+        {
+            edges[i][j]=0;
+        }
+    }
+    for(int i=0;i<e;i++)
+    {
+        int f,s;
+        cin >> f >> s;
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+    cout<<solve(edges,n);
+    return 0;
+}
